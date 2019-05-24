2e2e16ce1cd1a493ed89fced4535814580041efd
nehaejaz <neha.ejaz@icloud.com>
2019-05-25 04:04:05 +0500

    commiting basic formik form

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import FormikForm from './components/FormikForm'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <FormikForm />
+      </div>
+    );
+  }
+}
+
+export default App
